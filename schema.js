a38a419a3617108c0d6f1c0e7016d76444a25649
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-09-11 15:41:43 +0500

    Create basic enivroment

---
+++ schema.js
@@ -0,0 +1,44 @@
+import { gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    users: [User]
+    user(id: ID!): User
+    quotes: [Quote]
+    iquote(by: ID!): [Quote]
+  }
+  type User {
+    _id: ID!
+    firstName: String
+    lastName: String
+    email: String
+    quotes: [Quote]
+  }
+  type Quote {
+    name: String
+    by: ID
+  }
+
+  type Token {
+    token: String!
+  }
+
+  input SignUp {
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
+  }
+
+  input Login {
+    email: String!
+    password: String!
+  }
+
+  type Mutation {
+    createUser(signUp: SignUp!): User
+    login(login: Login!): Token
+  }
+`;
+
+export default typeDefs;
